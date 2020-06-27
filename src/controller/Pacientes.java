ea2fbc712bfd76450169e70224d170752ac21bed
Pedro <Pedro@PEDROPC>
2020-06-27 14:18:57 -0300

    Reformulação do Paciente

--- src/controller/Pacientes.java
+++ src/controller/Pacientes.java
@@ -2,26 +2,45 @@ package controller;
 
 public class Pacientes {
 
-    String nomecompleto;
-    int cpf;
-    String email;
+    private String nomecompleto;
+    private int rg;
+    private String email;
 
-    String data;
-    String hora;
-    String atividade; //eh oq ele vai fazer la
 
     // construtor	 
     Pacientes() {
-        this("", 0, "", "", "", "");
+        super();
     }
 
-    Pacientes(String nome_completo, int num_cpf, String email_pac, String data_con, String hora_con, String atividade_con) {
-        nomecompleto = nome_completo;
-        cpf = num_cpf;
-        email = email_pac;
-        data = data_con;
-        hora = hora_con;
-        atividade = atividade_con;
 
-    }
+	public String getNomecompleto() {
+		return nomecompleto;
+	}
+
+
+	public void setNomecompleto(String nomecompleto) {
+		this.nomecompleto = nomecompleto;
+	}
+
+
+	public int getRg() {
+		return rg;
+	}
+
+
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    
 }
