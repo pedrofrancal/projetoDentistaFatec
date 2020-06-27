14f2000d17ab2323eadb9f982adf62994aef3919
Pedro <Pedro@PEDROPC>
2020-06-27 14:51:15 -0300

    Preparações do create e update

--- src/controller/Create.java
+++ src/controller/Create.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class Create {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+/*Elaborado por Pedro França
+ *Com o proposito de salvar o arquivo em entrada.txt
+ */
+
+public class Create {
+	/*Estrutura do txt
+	 *Separaremos com ; e então na leitura/criação saberemos
+	 *o que carregar na classe paciente
+	 *nome; telefone; email; rg
+	 */
+	
 }
