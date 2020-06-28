d29c2ff84c3217da1f1ed7f15a6db0848df6b8ac
Pedro <Pedro@PEDROPC>
2020-06-28 14:21:18 -0300

    Create funcional

--- src/controller/Create.java
+++ src/controller/Create.java
@@ -10,20 +10,40 @@ import java.io.PrintWriter;
  */
 
 public class Create {
-	/*Estrutura do txt
-	 *Separaremos com ; e então na leitura/criação saberemos
-	 *o que carregar na classe paciente
-	 *nome; telefone; email; rg
+	/*
+	 * Estrutura do txt Separaremos com ; e então na leitura/criação saberemos o que
+	 * carregar na classe paciente nome; telefone; email; rg
 	 */
-	public void gravarTxt(Pacientes[] paciente) {
+
+	// Já incluido do create existe a possibilidade de atualizar o arquivo sem
+	// perdas
+	public void gravarTxt(Pacientes[] paciente) throws IOException {
 		File arq = new File(System.getProperty("user.home") + "/Desktop", "entrada.txt");
+		File dir = new File(System.getProperty("user.home") + "/Desktop");
 		StringBuffer buffer = new StringBuffer();
-		for(Pacientes p : paciente) {
-			buffer.append(p.getNomecompleto()+";");
-			buffer.append(p.getTelefone()+";");
-			buffer.append(p.getEmail()+";");
-			buffer.append(p.getRg());
-			buffer.append("\n");
+
+		if (dir.exists() && dir.isDirectory()) {
+			boolean existe = false;
+			if (arq.exists()) {
+				existe = true;
+			}
+			for (Pacientes p : paciente) {
+				buffer.append(p.getNomecompleto() + ";");
+//				buffer.append(p.getTelefone()+";");
+				buffer.append(p.getEmail() + ";");
+				buffer.append(p.getRg());
+				buffer.append("\n");
+			}
+			String conteudo = buffer.toString();
+			FileWriter fileWriter = new FileWriter(arq, existe);
+			PrintWriter print = new PrintWriter(fileWriter);
+			print.write(conteudo);
+			print.flush();
+			print.close();
+			fileWriter.close();
+		} else {
+			throw new IOException("Diretorio invalido");
 		}
+
 	}
 }
